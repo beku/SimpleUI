e131891ad1a3d6c93604e52cb0dc01a54f3c2316
Kai-Uwe Behrmann <ku.b@gmx.de>
2012-12-10 07:27:28 +0100

    place C guards in io header

--- io/io.h
+++ io/io.h
@@ -1,4 +1,14 @@
 #ifndef SIMPLE_UI_IO_H
 #define SIMPLE_UI_IO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
 char * sReadFileToMem(const char *filename);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
 #endif
